6cb03272099305fdcc7d270e04d1edce2e7fc109
Jérémie BARTOLI <jeremie.bartoli@free.fr>
2015-11-13 16:00:51 +0100

    Install & test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var dominos = require('pizzapi/dominos-pizza-api');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  pizzapi.Util.findNearbyStores(
+      '69007',
+      'Delivery',
+      function(storeData) {
+        console.log('\n\n##################\nNearby Stores\n##################\n\n',storeData.result.Stores);
+      }
+  );
+
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
