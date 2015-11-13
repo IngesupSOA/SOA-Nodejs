77eb4ef133a07dee41a3c03f75b74aee944a389b
Jérémie BARTOLI <jeremie.bartoli@free.fr>
2015-11-13 15:36:12 +0100

    Remove .idea from previous push

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /*
+   var pizzapi=require('dominos'); // or without payment option : var pizzapi=require('pizzapi');
+
+   pizzapi.Util.findNearbyStores(
+   '63102',))
+   'Delivery',
+   function(storeData){
+   console.log(storeData);
+   }
+   );
+   */
+
+
   res.render('index', { title: 'Express' });
 });
 
