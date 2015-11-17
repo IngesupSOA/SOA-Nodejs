dc805794eb9fed544fb1a3e1cacf2178a9a5a027
amaitre <antoine.maitre@ynov.com>
2015-11-17 20:26:45 +0100

    Updated app.js => adding setup route

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var swig = require('swig');
 
-var userDB = require('./models/UserDB.js');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config.js'); // get our config file
 
-var routes = require('./routes/index');
+//var User = require('User');
+var User = require('./Models/UserDB');//.schema;
+
+var index = require('./routes/index');
 var users = require('./routes/users');
 var signUp = require('./routes/Security/signUp');
 var authenticate = require('./routes/Security/authenticate');
+//var setup = require('./routes/setup');
 
 var app = express();
+
 
 // This is where all the magic happens!
 app.engine('html', swig.renderFile);
@@ -27,15 +34,37 @@ app.set('view cache', false);
 // To disable Swig's cache, do the following:
 swig.setDefaults({ cache: false });
 
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
+mongoose.connect(config.database, function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Connected to the database");
+  }
+});
+app.set('superSecret', config.secret); // secret variable
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+//// set up CORS resource sharing
+//app.use(function(req, res, next){
+//  res.header('Access-Control-Allow-Origin', '*');
+//  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//  next();
+//});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
+//app.use('/setup', setup);
 app.use('/users', users);
 app.use('/signUp', signUp);
 app.use('/authenticate', authenticate);
@@ -70,6 +99,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
