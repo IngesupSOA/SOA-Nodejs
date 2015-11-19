2c9e70147e6ca35df77966007dc35aef9cdd2b2e
Jérémie BARTOLI <jeremie.bartoli@free.fr>
2015-11-19 09:41:28 +0100

    Pushed current update with CSS starter auto-build

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var swig = require('swig');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var pizza = require('./routes/pizza');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/pizza', pizza);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
