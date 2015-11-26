3d49c256a314dfadc77a290bf71aa3e95f654939
TDoret <thomas.doret@gmail.com>
2015-11-26 22:04:42 +0100

    Correctif API dominos(affichage)

--- api/routes/default.js
+++ api/routes/default.js
@@ -73,31 +73,6 @@ module.exports = function () {
         }
     });
     router.route("/login").get(function (req, res) {
-
-        /*var optionsget = {
-            host : 'services.dominos.com.au',
-            path : '/Rest/fr/menus/31740/products',
-            method : 'GET'
-        };
-        var getData = https.request(optionsget, function(data) {
-            var body = '';
-            data.on('data', function(resx) {
-                body += resx;
-            });
-            data.on('end', function() {
-                console.log('ending http request');
-                var bodyParsed = JSON.parse(body);
-                var pizzas = bodyParsed.MenuPages[1].SubMenus;
-                res.render('pizza', { menus: pizzas });
-            });
-        });
-
-        getData.end();
-
-        getData.on('error', function(e) {
-            console.error("Error: "+e);
-        });*/
-
         return res.render('login', {title: 'Login'});
     });
 
--- api/routes/pizza.js
+++ api/routes/pizza.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
+var https = require('https');
 // http://pizza.dominos.fr/base/Store/GetStore/{ZIPCode}
 // http://pizza.dominos.fr/la-carte/nos-pizzas
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('pizza', { title: 'pizza' });
+    var optionsget = {
+     host : 'services.dominos.com.au',
+     path : '/Rest/fr/menus/31740/products',
+     method : 'GET'
+     };
+     var getData = https.request(optionsget, function(data) {
+     var body = '';
+     data.on('data', function(resx) {
+     body += resx;
+     });
+     data.on('end', function() {
+     console.log('ending http request');
+     var bodyParsed = JSON.parse(body);
+     var pizzas = bodyParsed.MenuPages[1].SubMenus;
+     res.render('pizza', { menus: pizzas });
+     });
+     });
+
+     getData.end();
+
+     getData.on('error', function(e) {
+     console.error("Error: "+e);
+     });
 });
 
 module.exports = router;
