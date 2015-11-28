7340e8e1e6944c79430e80477f0a2791933be95c
TDoret <thomas.doret@gmail.com>
2015-11-28 20:56:29 +0100

    Ajout Panier

--- api/routes/pizza.js
+++ api/routes/pizza.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
+var Cookie = require('cookies');
 // http://pizza.dominos.fr/base/Store/GetStore/{ZIPCode}
 // http://pizza.dominos.fr/la-carte/nos-pizzas
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+
+    var orderCookie = new Cookie(req, res).get('order');
     var optionsget = {
         host: 'services.dominos.com.au',
         path: '/Rest/fr/menus/31740/products',
@@ -20,7 +23,10 @@ router.get('/', function (req, res, next) {
             console.log('ending http request');
             var bodyParsed = JSON.parse(body);
             var pizzas = bodyParsed.MenuPages[1].SubMenus;
-            res.render('pizza', {menus: pizzas});
+            if(orderCookie == undefined || orderCookie == null)
+                res.render('pizza', {menus: pizzas, orderCookies: ""});
+            else
+                res.render('pizza', {menus: pizzas, orderCookies: JSON.parse(orderCookie)});
         });
     });
 
