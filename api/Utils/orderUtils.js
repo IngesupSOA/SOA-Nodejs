fce8e481bb14e0ba32cef313d893353e1e43c96d
TDoret <thomas.doret@gmail.com>
2015-11-29 15:35:51 +0100

    Correctif Panier

--- api/Utils/orderUtils.js
+++ api/Utils/orderUtils.js
@@ -9,13 +9,31 @@ var User = mongoose.model('User');
 var Class = mongoose.model('Class');
 var Cookies = require("cookies");
 
-module.exports.addPizzaIntoOrder = function (pizza, orderToUpdate, req, res) {
+module.exports.addPizzaIntoOrder = function (pizza, orderToUpdate) {
 
     var PizzaListTemp = orderToUpdate.pizzaList;
     PizzaListTemp.push(pizza);
 
     Order.findOneAndUpdate(
           {_id: orderToUpdate._id}
+        , {updated_at: Date.now(),
+            pizzaList: PizzaListTemp
+        }
+        , {w:1}, function(err, order) {
+            if (err) throw err;
+        });
+
+    return orderToUpdate;
+};
+
+module.exports.deletePizzaIntoOrder = function (pizza, orderToUpdate) {
+
+    var PizzaListTemp = orderToUpdate.pizzaList;
+    var index = PizzaListTemp.indexOf(pizza);
+    PizzaListTemp.splice(index,1);
+
+    Order.findOneAndUpdate(
+        {_id: orderToUpdate._id}
         , {updated_at: Date.now(),
             pizzaList: PizzaListTemp
         }
